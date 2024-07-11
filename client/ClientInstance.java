e1a27d60979175960a8f2586889bafa629a19e1d
Akakikunda <benitaakakikunda@gmail.com>
2024-07-11 16:46:18 +0300

    add derializer: add a serializer of converting input commands into json objects for sending to the server

--- client/ClientInstance.java
+++ client/ClientInstance.java
@@ -1,25 +1,22 @@
 package client;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-
 public class ClientInstance {
     // define attributes for the ClientInstance object
     String hostname;
     int port;
+    String clientId;
 
     public ClientInstance(String hostname, int port) {
         // constructor class for the client instance
         this.hostname = hostname;
         this.port = port;
     }
-
     public void start() throws IOException {
         // Todo: create a parent menu
-
         // execute code for interacting with the server
         try (
                 Socket socket = new Socket(hostname, port);
@@ -27,13 +24,19 @@ public class ClientInstance {
                 PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
                 BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
         ) {
+            this.clientId = (String) socket.getInetAddress().getHostAddress();
+            Serializer serializer = new Serializer(false);
+
             System.out.println("Connection with server a success");
 
             // read command line input
+            System.out.print("[" + this.clientId + "] -> ");
             String userInput = consoleInput.readLine();
+            String serializedCommand = serializer.serialize(userInput);
 
             // send content to the server
             output.println(userInput);
+            output.println(serializedCommand);
 
             // print server response
             String response = input.readLine();
