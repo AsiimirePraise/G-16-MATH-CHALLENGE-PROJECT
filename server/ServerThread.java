2d6d348d7af99f05489845199633816e6296cea8
praiseGit34 <praiseasiimire38@gmail.com>
2024-07-11 17:12:57 +0300

    add streaming support for network thread handler

--- server/ServerThread.java
+++ server/ServerThread.java
@@ -1,7 +1,5 @@
 package server;
-
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,40 +12,35 @@ public class ServerThread {
         this.socket = socket;
         // define constructor for the Server thread
     }
-
     public JSONObject readUserInput(BufferedReader input) throws IOException {
         String clientInput;
         StringBuilder clientIn = new StringBuilder();
-
         // iterate until the end of the json data
         while ((clientInput = input.readLine()) != null) {
             clientIn.append(clientInput);
-
             if (clientInput.equals("}")) {
                 break;
             }
         }
-
         // load data into a json format
         JSONObject jsonObject = new JSONObject(clientIn.toString());
         return jsonObject;
     }
-
     public void start() {
         try (
                 BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 PrintWriter output = new PrintWriter(socket.getOutputStream(), true)
-        ){
+                ){
 
             // read user input
-            JSONObject clientRequest = this.readUserInput(input);
+            JSONObject clientRequest;
+            while ((clientRequest = this.readUserInput(input)) != null) {
+                System.out.println(clientRequest.get("type") + "\n" + clientRequest.toString());
+                System.out.println("server received: " + clientRequest.toString());
 
-            System.out.println(clientRequest.get("type") + "\n" + clientRequest.toString());
-            System.out.println("server received: " + clientRequest.toString());
-
-            // send content back to client
-            output.println("Server received -> " + clientRequest.toString());
-
+                // send content back to client
+                output.println("Server received -> " + clientRequest.toString());
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
