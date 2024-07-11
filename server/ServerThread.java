b6b7666be9a09147d04e2ee9f2227985b405192d
Akakikunda <benitaakakikunda@gmail.com>
2024-07-11 16:55:08 +0300

    add input stream reader: read all input from the user and load data into json

--- server/ServerThread.java
+++ server/ServerThread.java
@@ -1,19 +1,36 @@
 package server;
 
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.*;
-
-
 public class ServerThread {
     private Socket socket;
     // server thread properties
-
     public ServerThread(Socket socket) {
         this.socket = socket;
         // define constructor for the Server thread
+    }
+
+    public JSONObject readUserInput(BufferedReader input) throws IOException {
+        String clientInput;
+        StringBuilder clientIn = new StringBuilder();
+
+        // iterate until the end of the json data
+        while ((clientInput = input.readLine()) != null) {
+            clientIn.append(clientInput);
+
+            if (clientInput.equals("}")) {
+                break;
+            }
+        }
+
+        // load data into a json format
+        JSONObject jsonObject = new JSONObject(clientIn.toString());
+        return jsonObject;
     }
 
     public void start() {
@@ -22,12 +39,14 @@ public class ServerThread {
                 PrintWriter output = new PrintWriter(socket.getOutputStream(), true)
         ){
 
-            // read input from the client
-            String clientInput = input.readLine();
-            System.out.println("server received: " + clientInput);
+            // read user input
+            JSONObject clientRequest = this.readUserInput(input);
+
+            System.out.println(clientRequest.get("type") + "\n" + clientRequest.toString());
+            System.out.println("server received: " + clientRequest.toString());
 
             // send content back to client
-            output.println("Server received -> " + clientInput);
+            output.println("Server received -> " + clientRequest.toString());
 
 
         } catch (IOException e) {
@@ -35,7 +54,6 @@ public class ServerThread {
             throw new RuntimeException(e);
         }
         System.out.println("Thread started");
-
         // start a thread for communicating with the client continously
     }
 }
