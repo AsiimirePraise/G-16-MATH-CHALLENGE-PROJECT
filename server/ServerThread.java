a050c0a34c7dfb0777156201a67c18284a6a1a22
praiseGit34 <praiseasiimire38@gmail.com>
2024-07-11 17:20:13 +0300

    add finally block for socket thread exception to close socket

--- server/ServerThread.java
+++ server/ServerThread.java
@@ -1,17 +1,23 @@
 package server;
+
 import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.*;
+
+
 public class ServerThread {
     private Socket socket;
     // server thread properties
+
     public ServerThread(Socket socket) {
         this.socket = socket;
         // define constructor for the Server thread
     }
+    
     public JSONObject readUserInput(BufferedReader input) throws IOException {
         String clientInput;
         StringBuilder clientIn = new StringBuilder();
@@ -26,27 +32,31 @@ public class ServerThread {
         JSONObject jsonObject = new JSONObject(clientIn.toString());
         return jsonObject;
     }
-    public void start() {
+
+    public void start() throws IOException {
+
+        System.out.println("Thread started");
+
         try (
                 BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 PrintWriter output = new PrintWriter(socket.getOutputStream(), true)
                 ){
-
             // read user input
             JSONObject clientRequest;
             while ((clientRequest = this.readUserInput(input)) != null) {
-                System.out.println(clientRequest.get("type") + "\n" + clientRequest.toString());
-                System.out.println("server received: " + clientRequest.toString());
+                System.out.println(socket.getInetAddress().getHostAddress() + " - - " + clientRequest.toString());
 
                 // send content back to client
-                output.println("Server received -> " + clientRequest.toString());
+                output.println(clientRequest.toString());
             }
 
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
+        } finally {
+            socket.close();
         }
-        System.out.println("Thread started");
+
         // start a thread for communicating with the client continously
     }
 }
