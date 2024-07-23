65e34b031ed5755d065e411a32de4273975bfdd8
Akakikunda <benitaakakikunda@gmail.com>
2024-07-23 20:40:14 +0300

    add control structure for printing invalid commands before sending to server

--- client/ClientInstance.java
+++ client/ClientInstance.java
@@ -4,16 +4,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+
 public class ClientInstance {
     // define attributes for the ClientInstance object
     String hostname;
     int port;
     String clientId;
+
     public ClientInstance(String hostname, int port) {
         // constructor class for the client instance
         this.hostname = hostname;
         this.port = port;
     }
+
     public void start() throws IOException {
         // Todo: create a parent menu
         // execute code for interacting with the server
@@ -22,23 +25,30 @@ public class ClientInstance {
                 BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
                 BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
-            ) {
+        ) {
             this.clientId = (String) socket.getInetAddress().getHostAddress();
             Serializer serializer = new Serializer(false);
-
             System.out.println("Connection with server a success");
             System.out.print("[" + this.clientId + "] -> ");
             // read command line input
-
             // Continuously read from the console and send to the server
             String userInput;
             while ((userInput = consoleInput.readLine()) != null) {
+                // send command to the server
                 String serializedCommand = serializer.serialize(userInput);
+                if (serializedCommand.equals("Invalid command")) {
+                    System.out.println(serializedCommand);
 
-                output.println(serializedCommand);
+                } else {
+                    output.println(serializedCommand);
 
-                String response = input.readLine();
-                System.out.println("response: " + response);
+                    // read response here from the server
+                    String response = input.readLine();
+                    System.out.println("response: " + response);
+                }
+
+
+                // prompt for the next instruction
                 System.out.print("[" + this.clientId + "] -> ");
             }
         } catch (Exception e) {
