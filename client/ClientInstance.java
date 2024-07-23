aeb9db16982e7b46ca80c84c03a4c58d4e7d9765
frankogenrwoth <ogenrwothjimfrank@gmail.com>
2024-07-23 20:57:25 +0300

    use regular expressions for selecting between valid and invalid input

--- client/ClientInstance.java
+++ client/ClientInstance.java
@@ -1,15 +1,21 @@
 package client;
+
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.regex.Pattern;
 
 public class ClientInstance {
     // define attributes for the ClientInstance object
     String hostname;
     int port;
     String clientId;
+    boolean isStudent;
+    boolean isAuthenticated;
 
     public ClientInstance(String hostname, int port) {
         // constructor class for the client instance
@@ -17,8 +23,16 @@ public class ClientInstance {
         this.port = port;
     }
 
+    public static boolean isValid(String input) {
+        String regex = "^\\{.*\\}$";
+        Pattern pattern = Pattern.compile(regex, Pattern.DOTALL);
+
+        return pattern.matcher(input).matches();
+    }
+
     public void start() throws IOException {
         // Todo: create a parent menu
+
         // execute code for interacting with the server
         try (
                 Socket socket = new Socket(hostname, port);
@@ -27,7 +41,8 @@ public class ClientInstance {
                 BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
         ) {
             this.clientId = (String) socket.getInetAddress().getHostAddress();
-            Serializer serializer = new Serializer(false);
+            Serializer serializer = new Serializer(false, false);
+
             System.out.println("Connection with server a success");
             System.out.print("[" + this.clientId + "] -> ");
             // read command line input
@@ -36,15 +51,15 @@ public class ClientInstance {
             while ((userInput = consoleInput.readLine()) != null) {
                 // send command to the server
                 String serializedCommand = serializer.serialize(userInput);
-                if (serializedCommand.equals("Invalid command")) {
-                    System.out.println(serializedCommand);
-
-                } else {
+                if (isValid(serializedCommand)) {
                     output.println(serializedCommand);
 
                     // read response here from the server
                     String response = input.readLine();
                     System.out.println("response: " + response);
+
+                } else {
+                    System.out.println(serializedCommand);
                 }
 
 
--- client/Serializer.java
+++ client/Serializer.java
@@ -1,27 +1,45 @@
 package client;
+
 import org.json.JSONObject;
+
 public class Serializer {
     boolean isAuthenticated;
-    public Serializer(boolean isAuthenticated) {
+    boolean isStudent;
+
+    public Serializer(boolean isAuthenticated, boolean isStudent) {
         this.isAuthenticated = isAuthenticated;
+        this.isStudent = isStudent;
     }
+
     public String login(String[] arr) {
+        if (this.isAuthenticated) {
+            return "Session already authenticated";
+        }
+
         JSONObject obj = new JSONObject();
         obj.put("command", "login");
         obj.put("isAuthenticated", false);
         obj.put("tokens", arr);
+        obj.put("isStudent", false);
+
         return obj.toString(4);
     }
+
     public String register(String[] arr) {
         JSONObject obj = new JSONObject();
         obj.put("command", "register");
         obj.put("isAuthenticated", false);
         obj.put("tokens", arr);
+        obj.put("isStudent", true);
+
+
         return obj.toString(4);
     }
+
     public void logout() {
         this.isAuthenticated = false;
     }
+
     public String serialize(String command) {
         String[] tokens = command.split("\\s+");
         if (!isAuthenticated && tokens[0].equals("register")) {
@@ -33,14 +51,14 @@ public class Serializer {
             case "logout":
                 this.logout();
                 break;
-
             default:
                 return "Invalid command";
         }
         return "";
     }
-    public static void main (String[] args) {
-        Serializer sample = new Serializer(true);
+
+    public static void main(String[] args) {
+        Serializer sample = new Serializer(false, false);
         sample.serialize("login frank ogenrwothjimfrank@gmail.com");
     }
 }
