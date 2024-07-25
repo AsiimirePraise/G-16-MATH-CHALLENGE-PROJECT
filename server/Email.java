37acb7dd9a745bfce5eb961a150374575d7b6b89
Akakikunda <benitaakakikunda@gmail.com>
2024-07-25 08:17:20 +0300

    add email client for sending emails

--- server/Email.java
+++ server/Email.java
@@ -1,26 +1,61 @@
 package server;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
 public class Email {
+    String host = "smtp.gmail.com";
+    String username = "eelection3.portal@gmail.com";
+    String from = "eelection3.portal@gmail.com";
+    String password = "mngv mtyl wmzk exmi";
+    Session session;
+
     public Email() {
-        Properties prop = new Properties();
-        prop.put("mail.smtp.auth", true);
-        prop.put("mail.smtp.starttls.enable", "true");
-        prop.put("mail.smtp.host", "sandbox.smtp.mailtrap.io");
-        prop.put("mail.smtp.port", "25");
-        prop.put("mail.smtp.ssl.trust", "sandbox.smtp.mailtrap.io");
+        Properties properties = System.getProperties();
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+        String username = this.username;
+        String password = this.password;
+        this.session = Session.getInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+
+                return new PasswordAuthentication(username, password);
+
+            }
+
+        });
+        this.session.setDebug(true);
     }
 
-    public void sendSingleEmail(String message, String email) {
+    public void sendParticipantRegistrationRequestEmail(String to, String participantEmail, String username) throws MessagingException {
+        MimeMessage message = new MimeMessage(this.session);
+
+        message.setFrom(new InternetAddress(this.from));
+
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+        message.setSubject("Notification of registration under your school");
+
+        StringBuilder emailMessage = new StringBuilder();
+        emailMessage.append("New participant notification\n\n");
+        emailMessage.append("This message is to notify you of a new participant's request to register under your school\n\n");
+        emailMessage.append("The participant details are as below\n");
+        emailMessage.append("\tUsername: ").append(username).append("\n");
+        emailMessage.append("\temail: ").append(participantEmail).append("\n");
+        emailMessage.append("\nTo verify this participant please login into the command line and confirm with the commands\n");
+        emailMessage.append("\taccept with:-> confirm yes/no <username>\n");
+
+        message.setText(emailMessage.toString());
+
+        Transport.send(message);
 
     }
-
-    public void SendMultiple(String message, String[] addresses) {
-
-    }
-
-//    public static void main(String[] args) {
+//    public static void main(String[] args) throws MessagingException {
 //        Email email = new Email();
+//        email.sendParticipantRegistrationRequestEmail("ogenrwothjimfrank@gmail.com", "frank@gmail.com", "frank");
 //    }
 }
